8f2f3c67df3b8fe54be5bdd8f942139765aa17bf
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2017-08-14 08:27:59 -0700

    Rnadom changes from book tutorial

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,8 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Euler (answer)
+import Test.Assert (assert)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "You should add some tests."
+  assert (answer == 233168)
